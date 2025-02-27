7b8064d814fa6d5fde4837851ae3e89bcd801e61
nagagopikrishna <nagagopikrishna4@gmailcom>
2025-02-27 09:39:10 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+lower_bound = 1
+upper_bound = 100
+max_attempts = 10
+target_number = random.randint(lower_bound, upper_bound)
+
+for attempt in range(max_attempts):
+    guess = int(input(f"Guess the number between {lower_bound} and {upper_bound}: "))
+    if guess < target_number:
+        print("Too low!")
+    elif guess > target_number:
+        print("Too high!")
+    else:
+        print("Congratulations! You guessed the number!")
+        break
+
